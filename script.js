cfc77759cb20ed197f5a1d5b7ccd85b8a3c73151
WinixMC <105449451+WinixMC@users.noreply.github.com>
2023-08-07 03:19:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-// Get the ball, goal, interact button, and audio elements
+// Get the ball, goal, interact button, audio elements, and settings elements
 const ball = document.getElementById('ball');
 const goal = document.getElementById('goal');
 const interactButton = document.getElementById('interact-button');
 const backgroundMusic = document.getElementById('background-music');
+const soundRange = document.getElementById('sound-range');
+const musicRange = document.getElementById('music-range');
 
 // Set the initial position of the ball and goal
 let ballLeft = 0;
@@ -25,4 +27,36 @@ function moveRight() {
 // Check for collision between ball and goal
 function checkCollision() {
   if (ballLeft === goalLeft) {
-    alert('Congratulations!});
+    alert('Congratulations! You reached the goal!');
+  }
+}
+
+// Event listeners for arrow key presses
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'ArrowLeft') {
+    moveLeft();
+  } else if (event.key === 'ArrowRight') {
+    moveRight();
+  }
+});
+
+// Play or pause background music
+interactButton.addEventListener('click', function() {
+  if (backgroundMusic.paused) {
+    backgroundMusic.play();
+  } else {
+    backgroundMusic.pause();
+  }
+});
+
+// Adjust sound volume
+soundRange.addEventListener('input', function() {
+  const volume = parseFloat(soundRange.value);
+  ball.style.opacity = volume;
+});
+
+// Adjust music volume
+musicRange.addEventListener('input', function() {
+  const volume = parseFloat(musicRange.value);
+  backgroundMusic.volume = volume;
+});
