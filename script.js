7213ba15b6e50225769bf17f615567cdda460033
WinixMC <105449451+WinixMC@users.noreply.github.com>
2023-08-07 02:32:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,37 @@ const player = document.getElementById('player');
 const interactButton = document.getElementById('interact-button');
 const backgroundMusic = document.getElementById('background-music');
 
+let currentStage = 1;
+
 function startGame() {
   interactButton.style.display = 'none';
   player.style.display = 'block';
   backgroundMusic.play();
+  
+  loadStage(currentStage);
+}
+
+function loadStage(stage) {
+  clearStage();
+  
+  switch (stage) {
+    case 1:
+      // Code to set up and display Stage 1
+      player.style.backgroundColor = 'red';
+      break;
+    case 2:
+      // Code to set up and display Stage 2
+      player.style.backgroundColor = 'green';
+      break;
+    // Add more cases for additional stages
+    
+    default:
+      console.log("Invalid stage number");
+  }
+}
+
+function clearStage() {
+  player.style.backgroundColor = '';
 }
 
 document.addEventListener('keydown', (event) => {
