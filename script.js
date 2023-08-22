e167708c7c2df8829e5e98cd04438bb2617b118d
Linden Hartmann <105449451+WinixMC@users.noreply.github.com>
2023-08-22 14:59:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 function playGame() {
-    var selectElement = document.getElementById("gameSelect");
-    var selectedGame = selectElement.value;
+  var selectElement = document.getElementById("gameSelect");
+  var selectedGame = selectElement.value;
 
-    // Redirect the user to the selected game's page
-    if (selectedGame === "game1") {
-        window.location.href = "game.html"; {
-        } else if (selectedGame = "game2") {
-        window.location.href = "DragonX_V2_International.html";
- } else if (selectedGame === "game3") {
-        window.location.href = "Copy of Shadow_Client_en_US-BETA-2.0.html";
-} else if (selectedGame === "game4") {
-        window.location.href = "Shadow_Client_en_US.html";
-        else if (selectedGame === "game5") {
-        window.location.href = "1v1";
-
-    }
+  // Redirect the user to the selected game's page
+  if (selectedGame === "game1") {
+    window.location.href = "game.html";
+  } else if (selectedGame === "game2") {
+    window.location.href = "DragonX_V2_International.html";
+  } else if (selectedGame === "game3") {
+    window.location.href = "Copy of Shadow_Client_en_US-BETA-2.0.html";
+  } else if (selectedGame === "game4") {
+    window.location.href = "Shadow_Client_en_US.html";
+  } else if (selectedGame === "game5") {
+    window.location.href = "1v1.html";
+  }
 }
 
 var playButton = document.getElementById("playButton");
