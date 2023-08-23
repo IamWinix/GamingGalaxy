87422e34a71a4de8ad6525a0e15da1d7cd154e0e
Linden Hartmann <105449451+WinixMC@users.noreply.github.com>
2023-08-23 02:03:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,29 @@ function navigateTo(url) {
   window.location.href = url;
 }
 
-window.addEventListener("scroll", function() {
-  var scrollDown = document.querySelector(".scroll-down");
-  if (window.scrollY > 0) {
-    scrollDown.style.display = "none";
-  } else {
-    scrollDown.style.display = "block";
-  }
-});
+function debounce(func, delay) {
+  let timer;
+  return function() {
+    clearTimeout(timer);
+    timer = setTimeout(func, delay);
+  };
+}
+
+window.addEventListener(
+  "scroll",
+  debounce(function() {
+    var scrollDown = document.querySelector(".scroll-down");
+    if (window.scrollY > 0) {
+      scrollDown.style.display = "none";
+    } else {
+      scrollDown.style.display = "block";
+    }
+  }, 200) // Adjust the delay (in milliseconds) as needed
+);
 
 document.addEventListener("DOMContentLoaded", function() {
   const images = document.querySelectorAll(".images img");
-  
+
   images.forEach(function(image) {
     image.addEventListener("click", function() {
       openLightbox(image.src, image.alt);
@@ -31,7 +42,7 @@ function openLightbox(src, alt) {
     <span class="close-button">&times;</span>
   `;
   document.body.appendChild(lightbox);
-  
+
   const closeButton = document.querySelector(".close-button");
   closeButton.addEventListener("click", closeLightbox);
 }
