31d1038eb5e76313491387997d3524e8b942d012
Linden Hartmann <105449451+WinixMC@users.noreply.github.com>
2023-08-23 01:49:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,33 @@ window.addEventListener("scroll", function() {
     scrollDown.style.display = "block";
   }
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  const images = document.querySelectorAll(".images img");
+  
+  images.forEach(function(image) {
+    image.addEventListener("click", function() {
+      openLightbox(image.src, image.alt);
+    });
+  });
+});
+
+function openLightbox(src, alt) {
+  const lightbox = document.createElement("div");
+  lightbox.id = "lightbox";
+  lightbox.innerHTML = `
+    <div class="lightbox-content">
+      <img src="${src}" alt="${alt}">
+    </div>
+    <span class="close-button">&times;</span>
+  `;
+  document.body.appendChild(lightbox);
+  
+  const closeButton = document.querySelector(".close-button");
+  closeButton.addEventListener("click", closeLightbox);
+}
+
+function closeLightbox() {
+  const lightbox = document.querySelector("#lightbox");
+  document.body.removeChild(lightbox);
+}
