18b874126997d4a7c018eca45c25bfabbe2770c6
Linden Hartmann <105449451+WinixMC@users.noreply.github.com>
2023-08-22 22:27:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,13 @@
-function playGame() {
-  var selectElement = document.getElementById("gameSelect");
-  var selectedGame = selectElement.value;
+// Get all game elements
+const games = document.querySelectorAll('.game');
 
-  // Redirect the user to the selected game's page
-  if (selectedGame === "game1") {
-    window.location.href = "game.html";
-  } else if (selectedGame === "game2") {
-    window.location.href = "DragonX_V2_International.html";
-  } else if (selectedGame === "game3") {
-    window.location.href = "Copy of Shadow_Client_en_US-BETA-2.0.html";
-  } else if (selectedGame === "game4") {
-    window.location.href = "Shadow_Client_en_US.html";
-  } else if (selectedGame === "game5") {
-    window.location.href = "1v1.html";
-  }
-  } else if (selectedGame === "game6") {
-    window.location.href = "Run.html";
-  }
-}
-
-var playButton = document.getElementById("playButton");
-playButton.addEventListener("click", playGame);
+// Attach click event listener to each game
+games.forEach(game => {
+  game.addEventListener('click', () => {
+    // Get the URL from the game link
+    const url = game.querySelector('a').href;
+    
+    // Redirect to the URL
+    window.location.href = url;
+  });
+});
